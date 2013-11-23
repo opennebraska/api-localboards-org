2a25de8a4be53478bf92dbe90e66d3e881093229
Jay Hannah <jay@jays.net>
2013-11-23 17:16:27 -0600

    Full seeding of the simple columns of the boards table.

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,5 +1,5 @@
 class Board < ActiveRecord::Base
-  attr_accessible :title, :seats, :alternating_seats, :qualifications, :department_id, :term_length, :is_active, :state_id, :city_id, :county_id
+  attr_accessible :title, :seats, :alternating_seats, :qualifications, :department_id, :meeting_dates, :meeting_place, :meeting_time, :duties, :url, :term_length, :is_active, :state_id, :city_id, :county_id
   
   has_many :board_members
   has_many :departments
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,12 +12,28 @@ if (County.where(name: 'Douglas').empty? && City.where(name: 'Omaha').empty?)
 	CityCounty.create(city_id: city.id, county_id: county.id)
 end
 
-File.open("db/human_data_entry/omaha-boards - boards.tsv", "r").each_line do |line|
+File.open("db/human_data_entry/omaha-boards - boards.tsv", "r").each_line.with_index do |line, i|
+    next if i == 0 
 	temp = line.chop.split("\t")
+    temp.collect{|x| x.strip}
+    # p "...#{temp[0]}..."
+    # p "...#{temp[0].strip}..."
 	Board.create(
-		title:             temp[0],
+		title:             temp[0].strip,
 		seats:             temp[1],
-		alternating_seats: temp[2]
+		alternating_seats: temp[2],
+        qualifications:    temp[5],
+    #    department_id:     temp[11],  TODO 
+        meeting_dates:     temp[12],
+        meeting_place:     temp[14],
+        meeting_time:      temp[13],
+        duties:            temp[15],
+        url:               temp[17],
+    #    term_length:       temp[5],  TODD
+        is_active:         1
+    #    state_id:          temp[5],
+    #    county_id:         temp[5],
+    #    city_id:           temp[5],
 	)
 end
 
