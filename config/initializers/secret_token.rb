c7b733abbae2b1c796fb51904c401936af82f1ca
Noah Koch <noahtkoch@gmail.com>
2013-11-22 23:59:13 -0600

    Downgrade to Rails 3.2.14

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OrgOrg::Application.config.secret_token = 'b3f2e0527e3dd49eebfccb05f2ab55e428bfb3b8353a9f26e0ab21d2506405a6f46193cd5715897188a27e68239ebbd02485d02a445535a7026d0def4634acbd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+OrgOrg::Application.config.session_store :cookie_store, key: '_OrgOrg_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# OrgOrg::Application.config.session_store :active_record_store
