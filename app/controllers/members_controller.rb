171f0304988352635885c6d0748b0bac86152474
Noah Koch <noahtkoch@gmail.com>
2013-11-24 11:58:25 -0600

    find individual member by id in state

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -11,12 +11,18 @@ class MembersController < OptionsController
 
 	def show
 		inject_option_headers
-
-		if board = get_board(params[:board_id], params)
-			render json: RestResponse.success(board.members.where(id: params[:id]))
+		if params[:board_id]
+			if board = get_board(params[:board_id], params)
+				render json: RestResponse.success(board.members.where(id: params[:id]))
+			else
+				RestResponse.notFound('No board found')
+			end
 		else
-			RestResponse.notFound('No board found')
+			if member = Member.find(params[:id])
+				render json: RestResponse.success(member)
+			else
+				RestResponse.notFound('No board found')
+			end
 		end
-
 	end
 end
