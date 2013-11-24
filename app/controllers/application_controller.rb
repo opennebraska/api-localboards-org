a2d10c2952446f9a1e7a01d5b1c9099f8169cf96
Noah Koch <noahtkoch@gmail.com>
2013-11-23 18:32:42 -0600

    work dammit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   after_filter :set_access_control_headers
 
-	def set_access_control_headers 
-		headers['Access-Control-Allow-Origin'] = 'http://localhost:3000' 
-		headers['Access-Control-Request-Method'] = '*' 
-	end
+  def set_access_control_headers 
+  	headers['Access-Control-Allow-Origin'] = 'http://localhost:3000' 
+      headers['Content-Type'] = 'application/json'
+  	headers['Access-Control-Request-Method'] = '*' 
+  end
 
 
 end
