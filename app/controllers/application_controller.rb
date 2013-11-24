6bd2d8b733f4bf24a92a2be948531784ebf676a6
Noah Koch <noahtkoch@gmail.com>
2013-11-23 18:36:31 -0600

    work dammit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,7 @@ class ApplicationController < ActionController::Base
   def set_access_control_headers 
   	headers['Access-Control-Allow-Origin'] = 'http://localhost:3000' 
       headers['Content-Type'] = 'application/json'
-  	headers['Access-Control-Request-Method'] = '*' 
+  	headers['Access-Control-Request-Method'] = '*'
+      headers['Access-Control-Request-Headers'] = '*' 
   end
-
-
 end
