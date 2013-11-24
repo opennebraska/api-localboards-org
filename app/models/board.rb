a4f9f2783c17888c62397547bca202ba505e2134
Jay Hannah <jay@jays.net>
2013-11-23 18:13:17 -0600

    Boards now map to Department IDs

--- app/models/board.rb
+++ app/models/board.rb
@@ -2,11 +2,11 @@ class Board < ActiveRecord::Base
   attr_accessible :title, :seats, :alternating_seats, :qualifications, :department_id, :meeting_dates, :meeting_place, :meeting_time, :duties, :url, :term_length, :is_active, :state_id, :city_id, :county_id
   
   has_many :board_members
-  has_many :departments
   has_many :members, through: :board_members
   has_many :board_seats
   
   belongs_to :state
   belongs_to :county
   belongs_to :city
+  belongs_to :department
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,29 +12,46 @@ if (County.where(name: 'Douglas').empty? && City.where(name: 'Omaha').empty?)
 	CityCounty.create(city_id: city.id, county_id: county.id)
 end
 
+
+
+
+my_boards = []
+my_departments = {}
 File.open("db/human_data_entry/omaha-boards - boards.tsv", "r").each_line.with_index do |line, i|
     next if i == 0 
 	temp = line.chop.split("\t")
-    temp.collect{|x| x.strip}
+    temp = temp.collect{|x| x.strip}
     # p "...#{temp[0]}..."
     # p "...#{temp[0].strip}..."
-	Board.create(
-		title:             temp[0].strip,
-		seats:             temp[1],
-		alternating_seats: temp[2],
-        qualifications:    temp[5],
-    #    department_id:     temp[11],  TODO 
-        meeting_dates:     temp[12],
-        meeting_place:     temp[14],
-        meeting_time:      temp[13],
-        duties:            temp[15],
-        url:               temp[17],
-    #    term_length:       temp[5],  TODD
+    my_boards << temp
+    my_departments[temp[11].upcase] = 1
+end
+
+my_departments.each_key do |key|
+    d = Department.create(
+        name: key
+    )
+    my_departments[key] = d.id
+end
+
+my_boards.each do |board|
+    Board.create(
+        title:             board[0],
+        seats:             board[1],
+        alternating_seats: board[2],
+        qualifications:    board[5],
+        department_id:     my_departments[board[11].upcase], 
+        meeting_dates:     board[12],
+        meeting_place:     board[14],
+        meeting_time:      board[13],
+        duties:            board[15],
+        url:               board[17],
+    #    term_length:       board[5],  TODD
         is_active:         1
-    #    state_id:          temp[5],
-    #    county_id:         temp[5],
-    #    city_id:           temp[5],
-	)
+    #    state_id:          board[5],
+    #    county_id:         board[5],
+    #    city_id:           board[5],
+    )
 end
 
 
