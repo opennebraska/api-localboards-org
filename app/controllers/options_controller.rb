0b57c047c6001214e095f486eb1c1b156f349de8
Noah Koch <noahtkoch@gmail.com>
2013-11-24 16:09:15 -0600

    sdlkjljsadfjklsdffsda

--- app/controllers/options_controller.rb
+++ app/controllers/options_controller.rb
@@ -10,9 +10,12 @@ class OptionsController < ApplicationController
     p 'start inject options'
     headers['Content-Type'] = 'application/json'
     # if request.remote_ip == '184.187.10.3'
+    p 'end conetent type'
     headers["Access-Control-Allow-Origin"] = request.env['HTTP_ORIGIN']
+    p 'end origin'
     headers["Access-Control-Allow-Headers"] = '*'
     # end
+    p 'end inject options'
   end
 
   def inject_cors_headers( )
