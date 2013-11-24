51ce6f23a9301abfed3c09f74869bc61313763ec
Noah Koch <noahtkoch@gmail.com>
2013-11-24 14:44:34 -0600

    Add active flags to members

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,11 +1,24 @@
 class MembersController < OptionsController
 	def index
 		inject_option_headers
-		
-		if board = get_board(params[:board_id], params)
-			render json: RestResponse.success(board.board_members.map{|m| m.member.attributes.merge({board_member_id: m.id, board_seat_id: m.board_seat_id})})
+		if params[:active] == 'false'
+			if board = get_board(params[:board_id], params)
+				render json: RestResponse.success(board.board_members.where(is_active: false).map{|m| m.member.attributes.merge({board_member_id: m.id, board_seat_id: m.board_seat_id})})
+			else
+				RestResponse.notFound('No board found')
+			end
+		elsif params[:active] == 'all'
+			if board = get_board(params[:board_id], params)
+				render json: RestResponse.success(board.board_members.map{|m| m.member.attributes.merge({board_member_id: m.id, board_seat_id: m.board_seat_id})})
+			else
+				RestResponse.notFound('No board found')
+			end
 		else
-			RestResponse.notFound('No board found')
+			if board = get_board(params[:board_id], params)
+				render json: RestResponse.success(board.board_members.where(is_active: true).map{|m| m.member.attributes.merge({board_member_id: m.id, board_seat_id: m.board_seat_id})})
+			else
+				RestResponse.notFound('No board found')
+			end
 		end
 	end
 
