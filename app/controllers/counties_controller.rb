bca7fb803cc2e20e15618217e0831bf64223d71e
Noah Koch <noahtkoch@gmail.com>
2013-11-24 18:11:14 -0600

    add county list

--- app/controllers/counties_controller.rb
+++ app/controllers/counties_controller.rb
@@ -1,5 +1,16 @@
 class CountiesController < OptionsController
 
+	def index 
+		inject_option_headers
+
+		if params[:state_id]
+			state, counties = get_jurisdiction(params[:state_id],nil)
+			render json: RestResponse.success(counties)
+		else	
+			RestResponse.notFound('Invalid API query')
+		end
+	end
+
 	def show
 		inject_option_headers
 
@@ -17,10 +28,15 @@ class CountiesController < OptionsController
 
 	def get_jurisdiction(state,county,page=nil,page_start=nil)
 		state = State.where("lower(abbreviation) = ?", state.downcase ).first
-		state = State.where(id: state).first unless state
-		
-		county = County.where(name: county, state_id: state.id).first
-		county = County.where(id: county, state_id: state.id).first unless county
+		if county
+			county = County.where(id: county, state_id: state.id).first	
+		else
+			if state
+				county = state.counties
+			else
+				county = nil
+			end
+		end
 
 		if county
 			return [state,county]
